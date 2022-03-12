bc4d923214277b50c1f467868ce21f0fd5ca54f8
Philippe B <phiberberian@gmail.com>
2022-03-12 13:04:06 -0300

    refactor(typing): implement generics to Token class methods

--- src/compiler/front/lexer/Token.ts
+++ src/compiler/front/lexer/Token.ts
@@ -8,15 +8,15 @@ export class Token<T, E = Tag> {
     span: Span
     tag: E
 
-    public constructor(content: T, source: string, span: Span, tag: Tag) {
+    public constructor(content: T, source: string, span: Span, tag: E) {
         this.content = content
         this.source = source
         this.span = span
         this.tag = tag
     }
 
-    public copy(): Token<T> {
-        return new Token<T>(this.content, this.source, this.span.copy(), this.tag)
+    public copy(): Token<T, E> {
+        return new Token<T, E>(this.content, this.source, this.span.copy(), this.tag)
     }
 
 }
